35f0d19ed3f6498c3bc090f426b99470474b02e7
Adiks <apacholak97@interia.pl>
2022-07-27 09:06:13 +0200

    progressing state from New to Pre_Auth

---
+++ src/test/java/com/adrian/msscssm/services/PaymentServiceImplTest.java
@@ -0,0 +1,42 @@
+package com.adrian.msscssm.services;
+
+import com.adrian.msscssm.domain.Payment;
+import com.adrian.msscssm.repository.PaymentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+@SpringBootTest
+class PaymentServiceImplTest {
+
+    @Autowired
+    PaymentService paymentService;
+
+    @Autowired
+    PaymentRepository paymentRepository;
+
+    Payment payment;
+
+    @BeforeEach
+    void setUp() {
+        payment = Payment.builder()
+                .amount(new BigDecimal("12.99"))
+                .build();
+    }
+
+    @Transactional
+    @Test
+    void preAuth() {
+        Payment savedPayment = paymentService.createNewPayment(payment);
+
+        paymentService.preAuth(savedPayment.getId());
+
+        Payment preAuthPayment = paymentRepository.getReferenceById(savedPayment.getId());
+
+        System.out.println(preAuthPayment);
+    }
+}
